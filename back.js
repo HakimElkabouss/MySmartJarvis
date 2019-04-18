51953fe4c9288b09082dc4f4f2c34931b59b64c8
HakimElkabouss <elkabousshakim@hotmail.com>
2019-04-18 14:27:27 +0200

    modified

--- back.js
+++ back.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient;
 
 const dbFile = 'post.db';
 
@@ -27,7 +27,9 @@ db.serialize( () => {
         
         db.run('CREATE TABLE IF NOT EXISTS Agenda (agenda_id INTEGER PRIMARY KEY AUTOINCREMENT, date DATETIME, agenda_name TEXT UNIQUE, agenda_user TEXT, agenda_lieu TEXT)');
 
-        db.run('CREATE TABLE IF NOT EXISTS Listes (liste_id INTEGER PRIMARY KEY AUTOINCREMENT, liste_name TEXT, liste_desc1 TEXT, liste_desc2 TEXT, liste_desc3 TEXT, liste_desc4 TEXT, liste_desc5 TEXT, liste_desc6 TEXT, liste_user TEXT, liste_time DATETIME, liste_avatar TEXT)');
+        db.run('CREATE TABLE IF NOT EXISTS Listes (liste_id INTEGER PRIMARY KEY AUTOINCREMENT, liste_name TEXT, liste_desc1 TEXT, liste_avatar1 TEXT, liste_desc2 TEXT, liste_avatar2 TEXT, liste_desc3 TEXT, liste_avatar3 TEXT, liste_desc4 TEXT, liste_avatar4 TEXT, liste_desc5 TEXT, liste_avatar5 TEXT, liste_desc6 TEXT, liste_avatar6 TEXT, liste_user1 TEXT, liste_user2 TEXT, liste_user3 TEXT, liste_user4 TEXT, liste_user5 TEXT, liste_user6 TEXT, liste_time DATETIME)');
+
+        db.run('CREATE TABLE IF NOT EXISTS Comments (comment_id INTEGER PRIMARY KEY AUTOINCREMENT, comment TEXT, comment_avatar TEXT, comment_date DATETIME)');
 
 })
 
@@ -46,6 +48,20 @@ app.post('/', function(request,response){
     });
 });
 
+app.get('/comment', function(request,response){
+   db.all('SELECT * FROM Comments', function(error, data){
+       response.send(data);
+   })
+})
+
+app.post('/comment', function(request,response){
+    db.run('INSERT INTO Comments (comment, comment_avatar, comment_date) VALUES (?, ?, ?)', request.body.comment, request.body.comment_avatar, request.body.comment_date, function(error, data){
+        console.log(request.body.comment);
+        console.log(request.body.comment_avatar);
+        console.log(request.body.comment_date);
+    })
+})
+ 
 app.get('/agenda', function(request,response){
     db.all('SELECT * FROM Agenda', function(error, data){
         response.send(data);
@@ -68,17 +84,28 @@ app.get('/listes', function(request,response){
 });
 
 app.post('/listes', function(request, response){
-    db.run('INSERT INTO Listes (liste_name, liste_desc1, liste_desc2, liste_desc3, liste_desc4, liste_desc5, liste_desc6, liste_user, liste_time, liste_avatar) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',request.body.liste_name, request.body.liste_desc1, request.body.liste_desc2, request.body.liste_desc3, request.body.liste_desc4, request.body.liste_desc5, request.body.liste_desc6, request.body.liste_user, request.body.liste_time, request.body.liste_avatar, function(error, data){
+    db.run('INSERT INTO Listes (liste_name, liste_desc1, liste_avatar1, liste_desc2, liste_avatar2, liste_desc3, liste_avatar3, liste_desc4, liste_avatar4, liste_desc5, liste_avatar5, liste_desc6, liste_avatar6, liste_user1, liste_user2, liste_user3, liste_user4, liste_user5, liste_user6, liste_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',request.body.liste_name, request.body.liste_desc1, request.body.liste_avatar1, request.body.liste_desc2, request.body.liste_avatar2, request.body.liste_desc3, request.body.liste_avatar3, request.body.liste_desc4, request.body.liste_avatar4, request.body.liste_desc5, request.body.liste_avatar5, request.body.liste_desc6, request.body.liste_avatar6, request.body.liste_user1, request.body.liste_user2, request.body.liste_user3, request.body.liste_user4, request.body.liste_user5, request.body.liste_user6, request.body.liste_time, function(error, data){
         console.log(request.body.liste_name);
         console.log(request.body.liste_desc1);
+        console.log(request.body.liste_avatar1);
         console.log(request.body.liste_desc2);
+        console.log(request.body.liste_avatar2);
         console.log(request.body.liste_desc3);
+        console.log(request.body.liste_avatar3);
         console.log(request.body.liste_desc4);
+        console.log(request.body.liste_avatar4);
         console.log(request.body.liste_desc5);
+        console.log(request.body.liste_avatar5);
         console.log(request.body.liste_desc6);
-        console.log(request.body.liste_user);
+        console.log(request.body.liste_avatar6);
+        console.log(request.body.liste_user1);
+        console.log(request.body.liste_user2);
+        console.log(request.body.liste_user3);
+        console.log(request.body.liste_user4);
+        console.log(request.body.liste_user5);
+        console.log(request.body.liste_user6);
         console.log(request.body.liste_time);
-        console.log(request.body.liste_avatar);
+        
     })
 })
 
--- front.js
+++ front.js
@@ -2,6 +2,7 @@ var container = document.getElementById('results');
 var contAg = document.getElementById('agenda_resultas');
 var contLi = document.getElementById('listes_resultas');
 
+
 $.get('http://localhost:8080/', function(response){
     response.forEach(function(po){
         const commentDiv = document.createElement('div');
@@ -11,23 +12,62 @@ $.get('http://localhost:8080/', function(response){
         const p = document.createElement('h4');
         const p1 = document.createElement('p');
         const av = document.createElement('img');
+        const hr = document.createElement('hr');
+        const imgCom = document.createElement('img');
+        const inputCom = document.createElement('input');
+        const sendCom = document.createElement('img');
 
-        h2.innerHTML = "<img src='https://cloud.mysmartjarvis.com/reseausocial/img/Modal1/message2.png' width='30'/>"+" "+"De "+po.post_name;
+        h2.innerHTML = "<img src='https://cloud.mysmartjarvis.com/reseausocial/img/Modal1/message2.png' width='30'/>"+" "+"de "+po.post_name;
         p1.innerHTML = po.post_time;
         p.innerHTML = po.post_post;
         av.setAttribute('src', po.post_avatar);
         av.setAttribute('width', "30");
+        imgCom.setAttribute('src','https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png');
+        imgCom.setAttribute('alt','Jean-Denis');
+        imgCom.setAttribute('class','pastilleMoyen');
+        imgCom.setAttribute('id','avCom');
+        imgCom.setAttribute('width','30');
+        inputCom.setAttribute('type','text');
+        inputCom.setAttribute('id','text1');
+        sendCom.setAttribute('src','https://cloud.mysmartjarvis.com/reseausocial/img/svg/envoyer.svg');
+        sendCom.setAttribute('width','30');
+        sendCom.setAttribute('id','envoyer1');
+        sendCom.setAttribute('onclick','sendCom()');
         
         commentDiv.setAttribute('id', 'comment');
         commentDiv.appendChild(h2);
         commentDiv.appendChild(av);
         commentDiv.appendChild(p1);
         commentDiv.appendChild(p);
+        commentDiv.appendChild(hr);
+        commentDiv.appendChild(imgCom);
+        commentDiv.appendChild(inputCom);
+        commentDiv.appendChild(sendCom);
         
     
     })
-
 });
+
+$.get('http://localhost:8080/comment', function(response){
+    response.forEach(function(com){
+        const comment = document.createElement('div');
+        container.appendChild(comment);
+
+        picCom = document.createElement('img');
+        textCom = document.createElement('h4');
+        dateCom = document.createElement('p');
+
+        picCom.setAttribute('src',com.comment_avatar);
+        picCom.setAttribute('width','30');
+        textCom.innerHTML = com.comment;
+        dateCom.innerHTML = com.comment_date;
+        comment.setAttribute('id','commentaire');
+
+        comment.appendChild(picCom);
+        comment.appendChild(textCom);
+        comment.appendChild(dateCom);
+    })
+})
 
 $.get('http://localhost:8080/agenda', function(response){
     response.forEach(function(ag){
@@ -57,20 +97,24 @@ $.get('http://localhost:8080/listes', function(response){
         const listDiv = document.createElement('div');
         contLi.appendChild(listDiv);
 
-        const h1 = document.createElement('h1');
         const h2 = document.createElement('h2');
         const desc1 = document.createElement('h3');
+        const av1 = document.createElement('img');
         const desc2 = document.createElement('h3');
+        const av2 = document.createElement('img');
         const desc3 = document.createElement('h3');
+        const av3 = document.createElement('img');
         const desc4 = document.createElement('h3');
+        const av4 = document.createElement('img');
         const desc5 = document.createElement('h3');
+        const av5 = document.createElement('img');
         const desc6 = document.createElement('h3');
+        const av6 = document.createElement('img');
         const p = document.createElement('p');
-        const av = document.createElement('img');
+        
 
         
         h2.innerHTML = "<img src='https://cloud.mysmartjarvis.com/reseausocial/img/Modal1/liste.png' width='30' />"+" "+li.liste_name;
-        h1.innerHTML = "Par "+li.liste_user;
         desc1.innerHTML = "<img src='images/unchecked.svg' width='25' id='unchecked' />"+" "+li.liste_desc1;
         desc2.innerHTML = "<img src='images/unchecked.svg' width='25' id='unchecked' />"+" "+li.liste_desc2;
         desc3.innerHTML = "<img src='images/unchecked.svg' width='25' id='unchecked' />"+" "+li.liste_desc3;
@@ -79,20 +123,46 @@ $.get('http://localhost:8080/listes', function(response){
         desc6.innerHTML = "<img src='images/unchecked.svg' width='25' id='unchecked' />"+" "+li.liste_desc6;
         p.innerHTML = li.liste_time;
 
-        av.setAttribute('src', li.liste_avatar);
-        av.setAttribute('width', "30");
-        desc1.setAttribute('id','descList');
+        av1.setAttribute('src', li.liste_avatar1);
+        av2.setAttribute('src', li.liste_avatar2);
+        av3.setAttribute('src', li.liste_avatar3);
+        av4.setAttribute('src', li.liste_avatar4);
+        av5.setAttribute('src', li.liste_avatar5);
+        av6.setAttribute('src', li.liste_avatar6);
+        av1.setAttribute('width', "30");
+        av1.setAttribute('id', "avatar");
+        av2.setAttribute('width', "30");
+        av2.setAttribute('id', "avatar");
+        av3.setAttribute('width', "30");
+        av3.setAttribute('id', "avatar");
+        av4.setAttribute('width', "30");
+        av4.setAttribute('id', "avatar");
+        av5.setAttribute('width', "30");
+        av5.setAttribute('id', "avatar");
+        av6.setAttribute('width', "30");
+        av6.setAttribute('id', "avatar");
+        desc1.setAttribute('id','descList1');
+        desc2.setAttribute('id','descList2');
+        desc3.setAttribute('id','descList3');
+        desc4.setAttribute('id','descList4');
+        desc5.setAttribute('id','descList5');
+        desc6.setAttribute('id','descList6');
         listDiv.setAttribute('id', 'liste');
         listDiv.appendChild(h2);
         listDiv.appendChild(p);
-        listDiv.appendChild(h1);
         listDiv.appendChild(desc1);
+        desc1.appendChild(av1);
         listDiv.appendChild(desc2);
+        desc2.appendChild(av2);
         listDiv.appendChild(desc3);
+        desc3.appendChild(av3);
         listDiv.appendChild(desc4);
+        desc4.appendChild(av4);
         listDiv.appendChild(desc5);
+        desc5.appendChild(av5);
         listDiv.appendChild(desc6);
-        listDiv.appendChild(av);
+        desc6.appendChild(av6);
+        
         })
 })
 
@@ -129,7 +199,7 @@ function send(){
 //     } 
 // }
 //     xhttp.open("GET", "http://localhost:8080/");
-//     xhttp.responseType = "text";
+//     // xhttp.responseType = "text";
 //     xhttp.send();
     
     
@@ -144,6 +214,40 @@ function send(){
     })
 }
 
+function sendCom(){
+    var name1 = document.querySelector('#select').value;
+    var pub1 = document.querySelector('#text1').value;
+
+    var ladate1 = new Date();
+    var h=ladate1.getHours();
+    if (h<10) {h = "0" + h}
+    var m=ladate1.getMinutes();
+    if (m<10) {m = "0" + m}
+    var time1 = h+":"+m+" - "+ladate1.getDate()+"/"+(ladate1.getMonth()+1)+"/"+ladate1.getFullYear();
+
+    var avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+
+    if(name1 == "Pascale"){
+        avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(name1 == "Elodie"){
+        avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(name1 == "Michael"){
+        avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    }else{
+        console.log('Rien');
+    }
+    
+    console.log(name1);
+    console.log(pub1);
+    console.log(time1);
+    console.log(avatar1);
+
+    $.post('http://localhost:8080/comment', {comment : pub1, comment_avatar : avatar1, comment_date : time1}, function(response){
+        console.log(response);
+    })
+
+}
+
 function sendAg(){
     var date = document.querySelector('#date').value;
     var name = document.querySelector('#eventName').value;
@@ -153,18 +257,24 @@ function sendAg(){
     $.post('http://localhost:8080/agenda', {agenda_name : name, agenda_user : user, date : date, agenda_lieu : lieu}, function(response){
         console.log(response);
     })
-
 }
+
+
 
 function sendLi(){
     var name = document.querySelector('#listName').value;
-    var user = document.querySelector('#selectUser').value;
     var desc1 = document.querySelector('#newTask').value;
+    var user1 = document.querySelector('#selectUser').value;
     var desc2 = document.querySelector('#newTask1').value;
+    var user2 = document.querySelector('#selectUser1').value;
     var desc3 = document.querySelector('#newTask2').value;
+    var user3 = document.querySelector('#selectUser2').value;
     var desc4 = document.querySelector('#newTask3').value;
+    var user4 = document.querySelector('#selectUser3').value;
     var desc5 = document.querySelector('#newTask4').value;
+    var user5 = document.querySelector('#selectUser4').value;
     var desc6 = document.querySelector('#newTask5').value;
+    var user6 = document.querySelector('#selectUser5').value;
 
     var ladate = new Date();
     var h=ladate.getHours();
@@ -173,19 +283,79 @@ function sendLi(){
     if (m<10) {m = "0" + m}
     var time = h+":"+m+" - "+ladate.getDate()+"/"+(ladate.getMonth()+1)+"/"+ladate.getFullYear();
 
-    var avatar = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+    var avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
 
-    if(user == "Pascale"){
-        avatar = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
-    }else if(user == "Elodie"){
-        avatar = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
-    }else if(user == "Michael"){
-        avatar = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    if(user1 == "Pascale"){
+        avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(user1 == "Elodie"){
+        avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(user1 == "Michael"){
+        avatar1 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
     }else{
         console.log('Rien');
     }
 
-    $.post('http://localhost:8080/listes', {liste_user : user, liste_name : name, liste_desc1 : desc1, liste_desc2 : desc2, liste_desc3 : desc3, liste_desc4 : desc4, liste_desc5 : desc5, liste_desc6 : desc6, liste_time : time, liste_avatar : avatar}, function(response){
+    var avatar2 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+
+    if(user2 == "Pascale"){
+        avatar2 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(user2 == "Elodie"){
+        avatar2 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(user2 == "Michael"){
+        avatar2 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    }else{
+        console.log('Rien');
+    }
+
+    var avatar3 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+
+    if(user3 == "Pascale"){
+        avatar3 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(user3 == "Elodie"){
+        avatar3 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(user3 == "Michael"){
+        avatar3 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    }else{
+        console.log('Rien');
+    }
+
+    var avatar4 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+
+    if(user4 == "Pascale"){
+        avatar4 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(user4 == "Elodie"){
+        avatar4 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(user4 == "Michael"){
+        avatar4 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    }else{
+        console.log('Rien');
+    }
+
+    var avatar5 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+
+    if(user5 == "Pascale"){
+        avatar5 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(user5 == "Elodie"){
+        avatar5 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(user5 == "Michael"){
+        avatar5 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    }else{
+        console.log('Rien');
+    }
+
+    var avatar6 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png";
+
+    if(user6 == "Pascale"){
+        avatar6 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png";
+    }else if(user6 == "Elodie"){
+        avatar6 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png";
+    }else if(user6 == "Michael"){
+        avatar6 = "https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png";
+    }else{
+        console.log('Rien');
+    }
+
+    $.post('http://localhost:8080/listes', {liste_user1 : user1, liste_user2 : user2, liste_user3 : user3, liste_user4 : user4, liste_user5 : user5, liste_user6 : user6, liste_name : name, liste_desc1 : desc1, liste_desc2 : desc2, liste_desc3 : desc3, liste_desc4 : desc4, liste_desc5 : desc5, liste_desc6 : desc6, liste_time : time, liste_avatar1 : avatar1,  liste_avatar2 : avatar2, liste_avatar3 : avatar3,  liste_avatar4 : avatar4, liste_avatar5 : avatar5, liste_avatar6 : avatar6}, function(response){
         console.log(response);
     })
 }
@@ -196,8 +366,13 @@ function sendLi(){
 $(document).ready(function(){
 
     $('#envoyer').click(function(){
-            $("input:text").val('');
-        });
+        $("input:text").val('');
+    });
+
+    $('#validList').click(function(){
+        $("input:text").val('');
+    })
+
     
     $('.Agenda').click(function(){
         $('#modal1').css('display', 'block');
@@ -262,31 +437,24 @@ $(document).ready(function(){
     })
 
     $('#Jean').click(function(){
-        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png" alt="Jean-Denis" class="pastilleMoyen">');
+        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/01.png" alt="Jean-Denis" style="width:30" class="pastilleMoyen">');
     })
 
     $('#Pascale').click(function(){
-        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png" alt="Pascale" class="pastilleMoyen"></img>');
+        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png" alt="Pascale" style="width:30" class="pastilleMoyen"></img>');
     })
 
     $('#Elodie').click(function(){
-        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png" alt="Elodie" class="pastilleMoyen"></img>');
+        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png" alt="Elodie" style="width:30" class="pastilleMoyen"></img>');
     })
 
     $('#Michael').click(function(){
-        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png" alt="Michael" class="pastilleMoyen"></img>');
+        $('.pastilleMoyen').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png" alt="Michael" style="width:30" class="pastilleMoyen"></img>');
     })
 
-   
+    // $('#descList1').click(function(){
+    //     $('#unchecked').replaceWith('<img src="https://cloud.mysmartjarvis.com/reseausocial/img/svg/checked.svg" width="30" />');
+    // })
 
 })
 
-// const name = document.querySelector('#select').value;
-
-// if(name == 'Pascale'){
-//     document.getElementById('main_User').innerHTML = '<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/04.png" alt="Pascale" class="pastilleMoyen"></img>';
-// }else if(name == 'Elodie'){
-//     document.getElementById('main_User').innerHTML = '<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/08.png" alt="Elodie" class="pastilleMoyen"></img>';
-// }else if(name == 'Michael'){
-//     document.getElementById('main_User').innerHTML = '<img src="https://cloud.mysmartjarvis.com/reseausocial/img/avatar/05.png" alt="Michael" class="pastilleMoyen"></img>';
-// }else{console.log('ok');}
